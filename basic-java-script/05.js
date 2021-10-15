56936656f3d6db2ffe993cde6b2252adff0c58d8
msseleznev <mikestm8979@gmail.com>
2021-10-15 08:13:04 +0400

    add test 02-35

---
+++ basic-java-script/05.js
@@ -0,0 +1,4 @@
+var a = 11;
+var b = 9;
+var c = 3;
+
---
+++ basic-java-script/06- 35.js
@@ -0,0 +1,105 @@
+
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3 ;
+c *= 10;
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"";
+console.log (myStr); // Change this line
+
+
+var myStr;
+myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+// Change this line
+
+var myStr;
+myStr = 'This is the first sentence.';
+myStr += ' This is the second sentence.';
+
+// Change code below this line
+
+var someAdjective  = "very interisting";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+
+/ Only change code below this line
+var myName = "Mike";
+var myStr = "My name is " + myName + " and I am well!" ;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line
+
+lastNameLength = lastName;
+lastNameLength = lastName.length;
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var thirdLetterOfLastName = lastName[2]; // Change this line
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "I'm looking " + myAdjective + " " + myNoun + " and " + myVerb + " very " + myAdverb + "."; // Change this line
+// Only change code above this line
+
+// Only change code below this line
+var myArray = ["cat", 0];
+
+// Only change code below this line
+var myArray = [["cat", 2], ["dog", 5]];
+
+var myArray = [50,60,70];
+let myData = myArray[0];
