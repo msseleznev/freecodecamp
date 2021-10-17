5edab076f68f1a44cc4dee35f73ee56095284e66
msseleznev <mikestm8979@gmail.com>
2021-10-17 22:47:21 +0400

    add folder ES6, add test 01-02.js

---
+++ ES6/01-02.js
@@ -0,0 +1,22 @@
+//01
+let catName;
+let quote;
+function catTalk() {
+    "use strict";
+
+    catName = "Oliver";
+    quote = catName + " says Meow!";
+
+}
+catTalk();
+
+//02
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+        let i = 'block scope';
+        console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+}
